c118078c9c4e10af3f49e696585933afb0c8626a
notcostheta <kaustubhbhiwsankar@gmail.com>
2023-03-15 01:22:52 +0530

    made a basic string concater

--- line_error.py
+++ line_error.py
@@ -1,25 +1,10 @@
-import re
-
-# Define the regular expression pattern
-pattern = r'^\s*(\d+)\s+([A-Z\d\s]+)\s+(\S+)\s*\n\s*([A-Z\d\s]+)\s+(\S+)\s*$'
-
-# Test string
-text = ' 13 82534 EN22153130 CHOUDHARY CHANDAN\nUPENDRAM OPEN ^ GOPENH 48.1419882'
-
-# Check if the text matches the pattern and extract the components
-match = re.match(pattern, text, flags=re.MULTILINE)
-if match:
-    # Extract the components
-    number1 = match.group(1)
-    string_sequence1 = match.group(2)
-    measurement1 = match.group(3)
-    string_sequence2 = match.group(4)
-    number2 = match.group(5)
-    
-    # Concatenate the components into a single line
-    new_text = f"{number1} {string_sequence1} {measurement1} {string_sequence2} {number2}"
-    
-    print(f"Original text:\n{text}")
-    print(f"\nNew text:\n{new_text}")
-else:
-    print("No match found.")
+string = " 13 82534 EN22153130 CHOUDHARY CHANDAN \nUPENDRAM OPEN ^ GOPENH 48.1419882"
+string_list = string.split(" ")
+print(string_list)
+gender = string_list[-5][-1]
+name_3 = string_list[-5]
+name_3 = name_3[:-1]
+string_list[-5] = name_3
+string_list.insert(-4, gender)
+string = " ".join(map(str, string_list))
+print(string)
