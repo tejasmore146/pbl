06965bb706d017a29106a2f9ffe20369c765d794
notcostheta <kaustubhbhiwsankar@gmail.com>
2023-03-17 01:15:16 +0530

    Done with making functions and main file

---
+++ all_code.py
@@ -0,0 +1,102 @@
+from PyPDF2 import PdfReader
+import re
+
+
+def extract(file, page_no):
+    '''
+    file: input pdf file
+    page_no: the page number that you want to scrape
+    makes a file.txt which has all the raw scraped data
+    '''
+    reader = PdfReader(file)
+    page = reader.pages[int(page_no)]
+
+    # Open a file in write mode
+    with open('file.txt', 'w') as f:
+        f.write(page.extract_text())
+
+
+def remove_header_footer(file):
+    '''
+    removes the top and bottom irrelevant text
+    aka crops the pdf file
+    '''
+    # Open the file for reading
+    with open(file, 'r') as f:
+        content = f.read()
+
+    # Define the regular expression patterns to match the strings
+    pattern1 = r"P\nrProvisional Allotment List of CAP Round -III for the Admission to the First Year Under Graduate \nTechnical Courses in Engineering and Technology \(4 Years \) &\nMaster of Engineering and Technology \(Integrated 5 Years \) Admissions A \.Y\. 2022-23State Common Entrance Test CellGovernment of Maharashtra\n?"
+    pattern2 = r"Legends for SeatType : H-Home University, O-Other than Home University, S-State Level, G-General, L-Ladies, AI-All India\n\* Green Color indicates Betterment in Choice Code , @ Blue Color indicates Betterment in Seat Type ,\nRed Color indicates No Change ,  \^ Gray Color indicates Admitted to Institute ,      &  Black Color indicates Newly Allotted\nMI-Minority Seats Allotted to Minority Candidates belonging to eligible Religious /Linguistic community\nMI-MH - Minority Seats Allotted to Non Minority Maharashtra Candidates , MI-AI - Minority Seats Allotted to Non Minority All India Candidates\nMerit No :  State General Merit No\. for Maharashtra Seats, All India Merit No\. for All India Seats"
+
+    # Use the regular expression patterns to replace all occurrences of the strings with an empty string
+    new_content = re.sub(pattern1, '', content, flags=re.MULTILINE | re.DOTALL)
+    new_content = re.sub(pattern2, '', new_content)
+
+    # Open the same file for writing and write the modified content
+    with open('output.txt', 'w') as f:
+        f.write(new_content)
+
+
+def remove_seat_type(file):
+    '''
+    Deletes the candidate Seat Type file
+    '''
+    # Define the text to delete
+    delete_patterns = [
+        r'Seat Type Candidate\s+CategoryGender Name of the Candidate Application ID Sr.\s+No\.Merit\s+No\.Meri'
+        # r'MHT-CET\s+Score'
+        # new logic
+    ]
+
+    # Read the contents of the file
+    with open(file, 'r') as f:
+        file_contents = f.read()
+
+    # Delete all occurrences of the target strings
+    for pattern in delete_patterns:
+        file_contents = re.sub(pattern, '', file_contents)
+
+    # Write the modified contents back to the file
+    with open('output.txt', 'w') as f:
+        f.write(file_contents)
+
+
+def fix_line_error(file, space_count):
+    '''
+    use space_count as 6 for first call, and 5 for second call
+    6 is used for when the name is 3 character long, and 5 is used for when the name is 4 character long
+
+    '''
+    with open(file, "r") as f:
+        lines = f.readlines()
+
+    new_lines = []
+    i = 0
+    while i < len(lines):
+        if lines[i].count(" ") == int(space_count):
+            current_line = lines[i].strip()
+            next_line = lines[i+1].strip()
+
+            # Converting the next_line to list
+            next_line_list = next_line.split(" ")
+            # pulling out the messed up gender from the line
+            gender = next_line_list[-5][-1]
+            # removing the extra gender character
+            gender_name_last = next_line_list[-5]
+            gender_name_last = gender_name_last[:-1]
+            # updating the string, and inserting the gender
+            next_line_list[-5] = gender_name_last
+            next_line_list.insert(-4, gender)
+            # convert the string_list to a string
+            next_line = " ".join(map(str, next_line_list))
+
+            joined_string = (current_line + " " + next_line)
+            new_lines.append(joined_string)
+            i += 2
+        else:
+            new_lines.append(lines[i].strip())
+            i += 1
+
+    with open("output.txt", "w") as f:
+        f.write("\n".join(new_lines))
---
+++ main.py
@@ -0,0 +1,17 @@
+from all_code import *
+import os
+
+if os.path.exists("output.txt"):
+    os.remove("output.txt")
+
+file = "CAPR-III_6207.pdf"
+
+extract(file, 4)
+file = "file.txt"
+remove_header_footer(file)
+file = "output.txt"
+remove_seat_type(file)
+file = "output.txt"
+fix_line_error(file, 6)
+file = "output.txt"
+fix_line_error(file, 5)
